4cf3fb8e85c9a9084cf1fe4f204fec9fca9211ea
maochunyi <maochunyi@163.com>
2020-04-21 21:13:35 +0800

    feat: 新增阿里easyExcel 导入导出工具

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # zdb-fast-mis
 zhidaobao's fast manage information system
+#开发环境
+
+语言：Java 8
+
+IDE(JAVA)： IDEA安装lombok插件
+
+IDE(前端)： WebStorm 或者 IDEA
+
+依赖管理：Maven
+
+数据库：MySQL5.7+
+
+代码管理：GIT
+
+缓存：Redis
+
+#后端
+
+基础框架：Spring Boot 2.x
+
+持久层框架：Mybatis-plus_3.1.2
+
+安全框架：Apache Shiro 1.4.0，Jwt_3.7.0
+
+数据库连接池：阿里巴巴Druid 1.1.10
+
+缓存框架：redis
+
+日志打印：logback
+
+其他：fastjson，poi、easyExcel，Swagger-ui，quartz, lombok（简化代码）等。
+
+#前端
+
+Vue 2.6.10,Vuex,Vue Router
+
+ant-design-vue
+
+
+
---
+++ src/test/java/com/zdb/ExcelImportTest.java
@@ -0,0 +1,42 @@
+package com.zdb;
+
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import lombok.SneakyThrows;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.apache.tomcat.util.file.ConfigFileLoader.getInputStream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExcelImportTest {
+	@SneakyThrows
+	@Test
+	public void testExcel2003NoModel() {
+		File f= new File("C:\\Users\\Administrator\\Desktop\\企业模板.xls") ;
+		InputStream inputStream = new FileInputStream(f);
+		try {
+			// 解析每行结果在listener中处理
+			ExcelListener listener = new ExcelListener();
+
+			ExcelReader excelReader = new ExcelReader(inputStream, ExcelTypeEnum.XLS, null, listener);
+			excelReader.read();
+		} catch (Exception e) {
+
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
